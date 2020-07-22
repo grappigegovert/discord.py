8533e5e697c04552ccc6aa05bb3d6807664f7413
Govert de Gans <grappigegovert@hotmail.com>
2020-07-22 16:23:21 +0200

    Don't use a namedtuple for Overwrites (0bc15fa)

--- discord/channel.py
+++ discord/channel.py
@@ -27,13 +27,27 @@ import copy
 from . import utils
 from .permissions import Permissions, PermissionOverwrite
 from .enums import ChannelType
-from collections import namedtuple
 from .mixins import Hashable
 from .role import Role
 from .user import User
 from .member import Member
 
-Overwrites = namedtuple('Overwrites', 'id allow deny type')
+class Overwrites:
+    __slots__ = ('id', 'allow', 'deny', 'type')
+
+    def __init__(self, **kwargs):
+        self.id = kwargs.pop('id')
+        self.allow = kwargs.pop('allow', 0)
+        self.deny = kwargs.pop('deny', 0)
+        self.type = kwargs.pop('type')
+
+    def _asdict(self):
+        return {
+            'id': self.id,
+            'allow': self.allow,
+            'deny': self.deny,
+            'type': self.type,
+        }
 
 class Channel(Hashable):
     """Represents a Discord server channel.
